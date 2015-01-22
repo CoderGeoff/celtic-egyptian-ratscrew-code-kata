e56a1f8114a983ecc1f57536215150ab031db9eb
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-22 15:05:11 +0000

    Add first failing test for DarkQueenSnapRule

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenSnapRule.cs
@@ -0,0 +1,10 @@
+﻿namespace CelticEgyptianRatscrewKata.Tests
+{
+    public sealed class DarkQueenSnapRule
+    {
+        public bool ContainsSnap(Stack stack)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/DarkQueenSnapRuleTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class DarkQueenSnapRuleTests
+    {
+        [Test]
+        public void ContainsSnap_IfGivenAnEmptyStack_ReturnsFalse()
+        {
+            // GIVEN
+            var rule = new DarkQueenSnapRule();
+
+            // WHEN
+            var emptyStack = new Stack(Enumerable.Empty<Card>());
+            var containsSnap = rule.ContainsSnap(emptyStack);
+
+            // THEN
+            Assert.False(containsSnap);
+        }
+    }
+}
