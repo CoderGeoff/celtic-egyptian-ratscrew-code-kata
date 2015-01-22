9861e7e97f07394c83efe121a2151544a59ebcd4
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-22 15:30:25 +0000

    Red: failing test for StandardSnapRule (empty stack test)

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/StandardSnapRuleTests.cs
@@ -0,0 +1,30 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class StandardSnapRuleTests
+    {
+        [Test]
+        public void ContainsSnap_IfGivenAnEmptyStack_ReturnsFalse()
+        {
+            // GIVEN
+            var rule = new StandardSnapRule();
+
+            // WHEN
+            var emptyStack = new Stack(Enumerable.Empty<Card>());
+            var containsSnap = rule.ContainsSnap(emptyStack);
+
+            // THEN
+            Assert.False(containsSnap);
+        }
+    }
+
+    public sealed class StandardSnapRule
+    {
+        public bool ContainsSnap(Stack stack)
+        {
+            return true;
+        }
+    }
+}
