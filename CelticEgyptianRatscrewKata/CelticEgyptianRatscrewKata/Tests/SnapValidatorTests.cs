f1eb230c73c947ff44cd8a859a8f159eb79b57bb
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 15:11:37 +0000

    Red: First failing test for SnapValidator with empty stack

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SnapValidatorTests.cs
@@ -0,0 +1,31 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class SnapValidatorTests
+    {
+        [Test]
+        public void SnapValidator__GivenAnEmptyStack_ShouldReturnFalse()
+        {
+            // GIVEN
+            var validator = new SnapValidator();
+
+            // WHEN
+            var stack = new Stack(Enumerable.Empty<Card>());
+            bool canSnap = validator.CanSnap(stack);
+
+            // THEN
+            Assert.False(canSnap);
+
+        }
+    }
+
+    public class SnapValidator
+    {
+        public bool CanSnap(Stack stack)
+        {
+            return true;
+        }
+    }
+}
