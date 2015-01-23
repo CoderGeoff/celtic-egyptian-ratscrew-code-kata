fb373ff56e337964386898c7279f23c93931a5ba
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 14:46:47 +0000

    Red: first failing test for sandwich snap rule

---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnapRule.cs
@@ -0,0 +1,10 @@
+﻿namespace CelticEgyptianRatscrewKata
+{
+    public class SandwichSnapRule
+    {
+        public bool ContainsSnap(Stack stack)
+        {
+            return true;
+        }
+    }
+}
---
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace CelticEgyptianRatscrewKata.Tests
+{
+    public class SandwichSnapRuleTests
+    {
+        [Test]
+        public void ContainsSnap_IfGivenAnEmptyStack_ReturnsFalse()
+        {
+            // GIVEN
+            var rule = new SandwichSnapRule();
+
+            // WHEN
+            var emptyStack = new Stack(Enumerable.Empty<Card>());
+            var containsSnap = rule.ContainsSnap(emptyStack);
+
+            // THEN
+            Assert.False(containsSnap);
+        }
+    }
+}
