888f089708873e5d490b74bf0fa1e7b7aca2a127
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 14:53:13 +0000

    Green: fix the test for the main case of Sandwich snap rule.

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnapRule.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/SandwichSnapRule.cs
@@ -1,10 +1,17 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿using System;
+using System.Linq;
+
+namespace CelticEgyptianRatscrewKata
 {
     public class SandwichSnapRule
     {
         public bool ContainsSnap(Stack stack)
         {
-            return false;
+            var cards = stack.ToList();
+            var everythingButTheFirstTwoCards = stack.Skip(2).ToList();
+
+            return cards.Zip(everythingButTheFirstTwoCards, (c1, c2) => new Tuple<Card, Card>(c1, c2))
+                        .Any(t => t.Item1.HasSameRank(t.Item2));
         }
     }
 }
