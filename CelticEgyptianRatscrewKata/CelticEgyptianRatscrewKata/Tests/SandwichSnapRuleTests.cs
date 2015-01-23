dc0d470bba19e93d991c6e520cc75d45beb9a9f7
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 14:51:23 +0000

    REd: Failing test for the main case for Sandwich snap

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/Tests/SandwichSnapRuleTests.cs
@@ -18,5 +18,24 @@ namespace CelticEgyptianRatscrewKata.Tests
             // THEN
             Assert.False(containsSnap);
         }
+
+        [Test]
+        public void ContainsSnap_IfGivenASandwichSnapStack_ReturnsTrue()
+        {
+            // GIVEN
+            var rule = new SandwichSnapRule();
+
+            // WHEN
+            var stack = new Stack(new []
+                                        {
+                                            new Card(Suit.Clubs, Rank.Ace), 
+                                            new Card(Suit.Clubs, Rank.Two), 
+                                            new Card(Suit.Diamonds, Rank.Ace)
+                                        });
+            var containsSnap = rule.ContainsSnap(stack);
+
+            // THEN
+            Assert.True(containsSnap);
+        }
     }
 }
