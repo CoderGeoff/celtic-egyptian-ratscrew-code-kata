769f465c229a89673976ab1503f0ecbd6527a4b7
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 14:36:25 +0000

    Green: Implement standard snap rule

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnapRule.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnapRule.cs
@@ -1,10 +1,16 @@
-﻿namespace CelticEgyptianRatscrewKata
+﻿using System;
+using System.Linq;
+
+namespace CelticEgyptianRatscrewKata
 {
     public sealed class StandardSnapRule
     {
         public bool ContainsSnap(Stack stack)
         {
-            return false;
+            var cards = stack.ToList();
+            var everythingButTheFirstCard = stack.Skip(1).ToList();
+
+            return cards.Zip(everythingButTheFirstCard, (c1, c2) => new Tuple<Card, Card>(c1, c2)).Any(t => t.Item1.Rank == t.Item2.Rank);
         }
     }
 }
