f723833f95d6ebc0c563add698b5951e17b56581
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-01-23 15:05:03 +0000

    Green: refactor to use common method in standard snap rule

--- CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnapRule.cs
+++ CelticEgyptianRatscrewKata/CelticEgyptianRatscrewKata/StandardSnapRule.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Linq;
+﻿using System.Linq;
 
 namespace CelticEgyptianRatscrewKata
 {
@@ -8,9 +7,7 @@ namespace CelticEgyptianRatscrewKata
         public bool ContainsSnap(Stack stack)
         {
             var cards = stack.ToList();
-            var everythingButTheFirstCard = stack.Skip(1).ToList();
-
-            return cards.Zip(everythingButTheFirstCard, (c1, c2) => new Tuple<Card, Card>(c1, c2)).Any(t => t.Item1.HasSameRank(t.Item2));
+            return cards.AnyCorrespondingCardsInTwoStacksWhere(cards.Skip(1), t => t.Item1.HasSameRank(t.Item2));
         }
     }
 }
