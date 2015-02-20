3132716d11e1cc66d2dac2904dc66be7431b1ecf
GeoffAtRedGate <Geoff.Bones@red-gate.com>
2015-02-20 15:41:40 +0000

    Implement commands

--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Commands/AttemptSnapCommand.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Commands/AttemptSnapCommand.cs
@@ -4,12 +4,18 @@ namespace ConsoleBasedGame.Commands
 {
     internal class AttemptSnapCommand : ICommand
     {
-        public AttemptSnapCommand(Player player, GameController game)
+        private readonly Player m_Player;
+        private readonly GameController m_GameController;
+
+        public AttemptSnapCommand(Player player, GameController gameController)
         {
+            m_Player = player;
+            m_GameController = gameController;
         }
 
         public void Execute()
         {
+            m_GameController.AttemptSnap(m_Player);
         }
     }
 }
--- CelticEgyptianRatscrewKata/ConsoleBasedGame/Commands/PlayCardCommand.cs
+++ CelticEgyptianRatscrewKata/ConsoleBasedGame/Commands/PlayCardCommand.cs
@@ -4,12 +4,18 @@ namespace ConsoleBasedGame.Commands
 {
     internal class PlayCardCommand : ICommand
     {
+        private readonly Player m_Player;
+        private readonly GameController m_Game;
+
         public PlayCardCommand(Player player, GameController game)
         {
+            m_Player = player;
+            m_Game = game;
         }
 
         public void Execute()
         {
+            m_Game.PlayCard(m_Player);
         }
     }
 }
